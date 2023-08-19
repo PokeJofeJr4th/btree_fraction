3c3d89723c91487459b2826e6a68dccd3ee8b8dd
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-08-19 08:37:37 -0400

    add max and min to UFrac8, add pi - 3 to examples/pi

--- examples/pi.rs
+++ examples/pi.rs
@@ -8,4 +8,7 @@ fn main() {
     println!("{pi16} = {pi16:?} = {}", UFrac16::PI);
     let pi32 = UFrac32::try_from(std::f64::consts::PI).unwrap();
     println!("{pi32} = {pi32:?} = {}", UFrac32::PI);
+
+    let pi_minus_3_32 = UFrac32::try_from(std::f64::consts::PI - 3.0).unwrap();
+    println!("{pi_minus_3_32} = {pi_minus_3_32:?}");
 }
